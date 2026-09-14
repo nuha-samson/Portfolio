b5247c3ca146216b89a2e1ca380649142d2e1ebd
Nuha Samson <nuhaskingdom@gmail.com>
2026-09-14 20:13:53 +0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <div><strong>NUHA</strong><span>SOFTWARE ENGINEER / CREATIVE DEVELOPER</span></div>
-      <button data-cursor="TOP" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>↑ BACK TO TOP</button>
-      <small>© 2026</small>
-    </footer>
-  );
-}
